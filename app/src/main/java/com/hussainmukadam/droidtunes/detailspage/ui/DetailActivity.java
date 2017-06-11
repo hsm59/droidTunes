67b2beaa7858fb6d8590d1f9e11e0ff774b5eb66
Hussain Mukadam <hussain@Hussains-MacBook-Pro.local>
2017-06-11 15:48:16 +0530

    Adds Card Views to Detail Activity

--- app/src/main/java/com/hussainmukadam/droidtunes/detailspage/ui/DetailActivity.java
+++ app/src/main/java/com/hussainmukadam/droidtunes/detailspage/ui/DetailActivity.java
@@ -1,5 +1,7 @@
 package com.hussainmukadam.droidtunes.detailspage.ui;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +13,9 @@ import android.widget.TextView;
 
 import com.hussainmukadam.droidtunes.R;
 import com.hussainmukadam.droidtunes.mainpage.models.Song;
+import com.hussainmukadam.droidtunes.utils.Util;
 import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
 
@@ -21,7 +25,7 @@ import java.util.ArrayList;
 
 public class DetailActivity extends AppCompatActivity {
     private static final String TAG = "DetailActivity";
-    ImageView iv_detail_artwork;
+    ImageView iv_detail_artwork, iv_track_url, iv_collection_url;
     TextView tv_detail_track_name, tv_detail_artist_name;
     Song song;
 
@@ -36,10 +40,33 @@ public class DetailActivity extends AppCompatActivity {
 
     private void initViews(){
         iv_detail_artwork = (ImageView) findViewById(R.id.iv_detail_artwork);
+        iv_track_url = (ImageView) findViewById(R.id.iv_track_url);
+        iv_collection_url = (ImageView) findViewById(R.id.iv_collection_url);
         tv_detail_artist_name = (TextView) findViewById(R.id.tv_detail_artist_name);
         tv_detail_track_name = (TextView) findViewById(R.id.tv_detail_track_name);
 
         Picasso.with(this).load(song.getArtworkUrl100()).into(iv_detail_artwork);
+        Target target = new Target() {
+            @Override
+            public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+                iv_track_url.setImageBitmap(Util.fastblur(bitmap, 1f, 10));
+                iv_collection_url.setImageBitmap(Util.fastblur(bitmap, 1f, 10));
+            }
+
+            @Override
+            public void onBitmapFailed(Drawable errorDrawable) {
+
+            }
+
+            @Override
+            public void onPrepareLoad(Drawable placeHolderDrawable) {
+
+            }
+        };
+        iv_track_url.setTag(target);
+        iv_collection_url.setTag(target);
+        Picasso.with(this).load(song.getArtworkUrl100()).into(target);
+
         tv_detail_artist_name.setText(song.getArtistName());
         tv_detail_track_name.setText(song.getTrackName());
     }
